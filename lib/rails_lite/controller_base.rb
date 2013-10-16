724231c747a89a0a09e5403c688c7c30a991b113
Isaac Murchie <imurchie@gmail.com>
2013-10-16 10:27:18 -0700

    Got render_content and redirect_to working

--- lib/rails_lite/controller_base.rb
+++ lib/rails_lite/controller_base.rb
@@ -5,19 +5,37 @@ require_relative 'session'
 class ControllerBase
   attr_reader :params
 
-  def initialize(req, res, route_params)
+  def initialize(req, res, route_params={})
+    @request      = req
+    @response     = res
+    @route_params = route_params
+
+    @response_built = false
   end
 
   def session
   end
 
   def already_rendered?
+    @response_built == true
   end
 
   def redirect_to(url)
+    raise "Already rendered content" if already_rendered?
+
+    @response_built = true
+
+    response.status = 302
+    response["Location"] = url
   end
 
-  def render_content(content, type)
+  def render_content(body, content_type)
+    raise "Already rendered content" if already_rendered?
+
+    @response_built = true
+
+    response.body = body
+    response.content_type = content_type
   end
 
   def render(template_name)
@@ -25,4 +43,8 @@ class ControllerBase
 
   def invoke_action(name)
   end
+
+
+  private
+    attr_reader :response
 end
